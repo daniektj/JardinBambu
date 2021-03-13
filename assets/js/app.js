205c794866b9e3e9089d851f863503f456000479
DaniekTj <imperio.hacker@gmail.com>
2021-03-13 04:55:53 -0800

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,31 @@
 var swiper = new Swiper('.swiper-container', {
-  lazy: true,
-  spaceBetween: 30,
-  centeredSlides: true,
-  effect: 'fade',
-  speed: 2000,
-  autoplay: {
-    delay: 4500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
+    lazy: true,
+    spaceBetween: 30,
+    centeredSlides: true,
+    effect: 'fade',
+    speed: 2000,
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 
-
+  
 
 document.querySelector('.section_04__gallery').onclick = (e) => {
-  let imageName = e.target.id;
-  
-  /* Validamos el click y si es verdadero procede*/
-  imageName ?     
-      basicLightbox.create(`
-      <img src="assets/images/${imageName}.jpg">
-  `).show()
-  :
-  console.log('falso');
+    let imageName = e.target.id;
+    
+    /* Validamos el click y si es verdadero procede*/
+    imageName ?     
+        basicLightbox.create(`
+        <img src="assets/images/${imageName}.jpg">
+    `).show()
+    :
+    console.log('falso');
 
 }; 
 
@@ -35,14 +35,14 @@ const boton = document.querySelector('.boton');
 const slide = document.querySelector(".cuadro");
 
 boton.onclick = () => {
-  numero = !numero
+    numero = !numero
 
-  numero ? 
-  slide.style.setProperty('transform', `translateX(0px)`) :
-  slide.style.setProperty('transform', `translateX(-300px)`);
+    numero ? 
+    slide.style.setProperty('transform', `translateX(0px)`) :
+    slide.style.setProperty('transform', `translateX(-250px)`);
 
-  numero ? 
-  boton.style.setProperty('background-image', `url('assets/images/mobile-menu-close.svg')`) :
-  boton.style.setProperty('background-image', `url('assets/images/mobile-menu-open.svg')`);
-  
+    numero ? 
+    boton.style.setProperty('background-image', `url('assets/images/mobile-menu-close.svg')`) :
+    boton.style.setProperty('background-image', `url('assets/images/mobile-menu-open.svg')`);
+    
 }
